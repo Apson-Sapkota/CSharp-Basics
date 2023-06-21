2f541318d92e97a3a49b2e0ec626e5f787f6c6b5
Apson Sapkota <109445874+Apson-Sapkota@users.noreply.github.com>
2023-06-21 19:06:06 +0545

    Add files via upload

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 21;
+            string name = "Apson";
+
+            Console.WriteLine("My name is "+name+". And My age is " + age);
+            Console.ReadLine();
+        }
+    }
+}
