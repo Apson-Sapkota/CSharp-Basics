4bffb437e7e9f0e2901b5a132195ace7e922b14c
Apson Sapkota <109445874+Apson-Sapkota@users.noreply.github.com>
2023-06-23 17:48:07 +0545

    Add files via upload

---
+++ getter and setter/Getter and Setter/Movie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Getter_and_Setter
+{
+    internal class Movie
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+        private string rating;
+
+        public Movie(string title, string author, int pages, string rating)
+        {
+            Title = title;
+            Author = author;
+            Pages = pages;
+            Rating = rating;
+        }
+
+        public string Rating
+        {
+            get { return rating; }
+            set
+            {
+                if (value == "G" || value == "GR")
+                {
+                    rating = value;
+                }
+                else { rating = "NR"; }
+            }
+        }
+    }
+}
---
+++ getter and setter/Getter and Setter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getter_and_Setter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie Movie1 = new Movie("Harry Potter","JK Rolling",223,"G");
+            Console.WriteLine(Movie1.Rating);
+            Console.ReadLine();
+        }
+    }
+}
