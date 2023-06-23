e05a6fc2025dcdf4fabefbe34836acf13ad4fa63
Apson Sapkota <109445874+Apson-Sapkota@users.noreply.github.com>
2023-06-23 16:26:20 +0545

    Add files via upload

---
+++ Class and object/Class and object/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_and_object
+{
+    internal class Book
+    {
+       
+        public string Title;
+        public string Author;
+        int Pages;
+    }
+}
---
+++ Class and object/Class and object/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_and_object
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book Book1 = new Book();
+            Book1.Title = "Harry Potter";
+            
+            
+            Console.Write(Book1.Title);
+
+            Book Book2 = new Book();
+            Console.ReadLine();
+        }
+    }
+}
