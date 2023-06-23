aa6699764d93c9e3417b48b61323c710b0f1ce15
Apson Sapkota <109445874+Apson-Sapkota@users.noreply.github.com>
2023-06-23 16:34:28 +0545

    Add files via upload

---
+++ Constructors/Constructors/Constructors1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Constructors1
+    {
+        public string Title;
+        public int Pages;
+        public string Author;
+
+        public Constructors1(string aTitle,int aPages,string aAuthor)
+        {
+            Title = aTitle;
+            Pages = aPages;
+            Author = aAuthor;
+        }
+    }
+}
---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Constructors1 Book1 = new Constructors1("Harry Potter",222,"Jk Rolling");
+            Console.WriteLine(Book1.Title);
+            Console.ReadLine();
+        }
+    }
+}
